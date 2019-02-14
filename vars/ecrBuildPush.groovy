b3d0c5165c8e226505b032ee7f51a39aee13a44f
stubrowncloudbees <33249923+stubrowncloudbees@users.noreply.github.com>
2019-02-14 16:39:05 +0000

    Update ecrBuildPush.groovy

--- vars/ecrBuildPush.groovy
+++ vars/ecrBuildPush.groovy
@@ -1,63 +1,72 @@
-def awsCreds = "ecr_push_pull"
-def awsRegion = "eu-west-1"
-def ecrRepo =  "024942195839.dkr.ecr.eu-west-1.amazonaws.com/stubrownuk123"
-def ecrTag = "latest"
-def awscliContainer = "mesosphere/aws-cli:1.14.5"
-def dockerContainer = "docker:18.06"
+def call(body) {
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
 
-pipeline {
-  agent {
-    kubernetes {
-      label 'dec_docker_build_and_publish'
-      defaultContainer 'jnlp'
-      yaml """
-apiVersion: v1
-kind: Pod
-metadata:
-  labels:
-    some-label: some-label-value
-spec:
-  containers:
-  - name: awscli
-    image: ${awscliContainer}
-    command:
-    - cat
-    tty: true
-  - name: docker
-    image: ${dockerContainer}
-    command: ["cat"]
-    tty: true
-    volumeMounts:
-    - mountPath: /var/run/docker.sock
-      name: docker-socket
-  volumes:
-  - name: docker-socket
-    hostPath:
-      path: /var/run/docker.sock
-      type: Socket
-"""
-    }
-  }
-  stages {
-      stage('Build and push Image') {
-          steps {
-              container('awscli'){
-                  withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: awsCreds, secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                      script{
-                          sh "aws ecr get-login --region ${awsRegion} --no-include-email > lgn"
-                          env.dockerLogin = readFile 'lgn'
+    def awsCreds = "ecr_push_pull"
+    def awsRegion = "eu-west-1"
+    def ecrRepo =  "024942195839.dkr.ecr.eu-west-1.amazonaws.com/stubrownuk123"
+    def ecrTag = "latest"
+    def awscliContainer = "mesosphere/aws-cli:1.14.5"
+    def dockerContainer = "docker:18.06"
+
+
+    pipeline {
+      agent {
+        kubernetes {
+          label 'dec_docker_build_and_publish'
+          defaultContainer 'jnlp'
+          yaml """
+    apiVersion: v1
+    kind: Pod
+    metadata:
+      labels:
+        some-label: some-label-value
+    spec:
+      containers:
+      - name: awscli
+        image: ${awscliContainer}
+        command:
+        - cat
+        tty: true
+      - name: docker
+        image: ${dockerContainer}
+        command: ["cat"]
+        tty: true
+        volumeMounts:
+        - mountPath: /var/run/docker.sock
+          name: docker-socket
+      volumes:
+      - name: docker-socket
+        hostPath:
+          path: /var/run/docker.sock
+          type: Socket
+    """
+        }
+      }
+      stages {
+          stage('Build and push Image') {
+              steps {
+                  container('awscli'){
+                      withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: awsCreds, secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                          script{
+                              sh "aws ecr get-login --region ${awsRegion} --no-include-email > lgn"
+                              env.dockerLogin = readFile 'lgn'
+                          }
                       }
                   }
-              }
-              container('docker'){
-                  sh "${env.dockerLogin}"
-                  sh "docker build . -t ${ecrRepo}:${ecrTag}"
-                  sh "docker push ${ecrRepo}:${ecrTag}"
+                  container('docker'){
+                      sh "${env.dockerLogin}"
+                      sh "docker build . -t ${ecrRepo}:${ecrTag}"
+                      sh "docker push ${ecrRepo}:${ecrTag}"
+                  }
               }
           }
       }
-  }
+    }
 }
 
 
