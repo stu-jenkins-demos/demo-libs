c107f555587739459eea9daeb91f20ee581b3224
stubrowncloudbees <sbrown@cloudbees.com>
2019-02-15 07:52:38 +0000

    some debug

--- vars/getValueOrDefault.groovy
+++ vars/getValueOrDefault.groovy
@@ -2,17 +2,17 @@ def call(String actualValue,String defaultValue) {
     echo "the autual value $actualValue "
     echo "the default value $defaultValue"
 
-    if (!actualValue){
-        print "its null test"
+    if (!actualValue?.trim()) {
+        echo "string is null or empty"
     }
-    //if (!actualValue?.trim()) {
-    if (!actualValue) {
+    if (!actualValue?.trim()) {
+
+
         echo "not set $actualValue"
         echo "sending defauly value $defaultValue"
         return defaultValue?.trim()
     }else{
         echo " set $actualValue"
-    }
     echo "sending actual value $actualValue"
     return actualValue?.trim()
 }
