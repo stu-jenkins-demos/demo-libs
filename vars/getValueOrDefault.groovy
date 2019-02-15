7e2c929eb261b0937246769b909ed3b93d024b65
stubrowncloudbees <sbrown@cloudbees.com>
2019-02-15 07:13:38 +0000

    some debug

--- vars/getValueOrDefault.groovy
+++ vars/getValueOrDefault.groovy
@@ -2,7 +2,9 @@ def call(String actualValue,String defaultValue) {
     echo "the autual value $actualValue "
     echo "the default value $defaultValue"
     if (!actualValue?.trim()) {
+        echo "sending $defaultValue"
         return defaultValue?.trim()
     }
+    echo "sending $actualValue"
     return actualValue?.trim()
 }
