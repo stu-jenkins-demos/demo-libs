8d1aa1f798a8ff6e43be236eb8fc9a5faba424f4
stubrowncloudbees <sbrown@cloudbees.com>
2019-04-04 13:24:39 +0100

    addind in initial api all

--- vars/apiTeamStatus.groovy
+++ vars/apiTeamStatus.groovy
@@ -20,9 +20,7 @@ def call(teamName, recipe) {
     }
     EOF
     }
-    #curl -s -u $CJOCUSER:$CJOCPASSWD https://cjejp.core.pscbdemos.com/cjoc/blue/rest/cjoc/teams/ -H "Content-Type: application/json" -d "\$(generate_post_data $TEAMNAME $RECIPE)"
-
-    #curl  -s -u $CJOCUSER:$CJOCPASSWD $CORECURL/cjoc/blue/rest/cjoc/teams/$lTEAMNAME/?pretty=true| jq '. | {mmstatus: .status}'
+   
     
     """
 
